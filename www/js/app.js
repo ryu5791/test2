5fa09caeb1876221211cf7b1c81ef27b8f5c71cc
ryu5791 <ryu5791@yahoo.co.jp>
2016-09-19 21:41:33 +0900

    詳細画面大まかに作成。 リストの同期がまだ

--- www/js/app.js
+++ www/js/app.js
@@ -6,7 +6,89 @@ var ncmb = new NCMB("15c1b1aa62fb0128a2b013dd7480250f71e00a80177d53e1cab99457a7d
 
 const ThisScoreTbl = "Score2016_2";
 
+var regMem;         // regular member
 
+window.onload = function(){
+        regMem = new Array();
+    regMem[0] = new set_regMem(1,"岩井","いわい");
+    regMem[1] = new set_regMem(2,"大谷","おおたに");
+	regMem[2] = new set_regMem(6,"木村","きむら");
+	regMem[3] = new set_regMem(10,"住野","すみの");
+	regMem[4] = new set_regMem(13,"中川","なかがわ");
+	regMem[5] = new set_regMem(17,"野上","のがみ");
+	regMem[6] = new set_regMem(18,"長谷川","はせがわ");
+	regMem[7] = new set_regMem(22,"前田","まえだ");
+	regMem[8] = new set_regMem(27,"矢澤","やざわ");
+	regMem[9] = new set_regMem(29,"山本修","やまもと");
+	regMem[10] = new set_regMem(31,"吉川","よしかわ");
+	regMem[11] = new set_regMem(32,"和田","わだ");
+	regMem[12] = new set_regMem(34,"大藤","おおふじ");
+	regMem[13] = new set_regMem(39,"濱田","はまだ");
+	regMem[14] = new set_regMem(40,"志水","しみず");
+	regMem[15] = new set_regMem(48,"中前","なかまえ");
+	regMem[16] = new set_regMem(49,"高橋","たかはし");
+	regMem[17] = new set_regMem(50,"田村","たむら");
+	regMem[18] = new set_regMem(52,"板倉","いたくら");
+	regMem[19] = new set_regMem(53,"上村","うえむら");
+	regMem[20] = new set_regMem(55,"榊","さかき");
+	regMem[21] = new set_regMem(62,"清見","きよみ");
+	regMem[22] = new set_regMem(63,"小嶋","こじま");
+	regMem[23] = new set_regMem(64,"古川","ふるかわ");
+	regMem[24] = new set_regMem(67,"栄里子","えり");
+	regMem[25] = new set_regMem(72,"植田","うえだ");
+	regMem[26] = new set_regMem(73,"一柳","いちりゅう");
+	regMem[27] = new set_regMem(74,"徳武","とくたけ");
+	regMem[28] = new set_regMem(75,"疋田","ひきだ");
+	regMem[29] = new set_regMem(76,"名越","なごし");
+	regMem[30] = new set_regMem(77,"宮田","みやた");
+	regMem[31] = new set_regMem(78,"ひろみ","ひろみ");
+	regMem[32] = new set_regMem(79,"キム","きむ");
+	regMem[33] = new set_regMem(80,"福山","ふくやま");
+	regMem[34] = new set_regMem(82,"片山","かたやま");
+	regMem[35] = new set_regMem(84,"宮脇","みやわき");
+	regMem[36] = new set_regMem(85,"大朝","おおとも");
+	regMem[37] = new set_regMem(87,"浪江","なみえ");
+	regMem[38] = new set_regMem(88,"齊藤","さいとう");
+	regMem[39] = new set_regMem(89,"日高","ひだか");
+	regMem[40] = new set_regMem(90,"田熊","たぐま");
+	regMem[41] = new set_regMem(91,"井関","いせき");
+	regMem[42] = new set_regMem(100,"角コーチ","すみ");
+	regMem[43] = new set_regMem(149,"菊池","きくち");
+	regMem[44] = new set_regMem(165,"大勝","おおかつ");
+	regMem[45] = new set_regMem(177,"古田","ふるた");
+	regMem[46] = new set_regMem(277,"南木","なんぼく");
+	regMem[47] = new set_regMem(281,"西崎","にしざき");
+    regMem[48] = new set_regMem(92,"大川","おおかわ");
+    regMem[49] = new set_regMem(93,"平岡","ひらおか");
+    regMem[50] = new set_regMem(66,"荒本","あらもと");
+    regMem[51] = new set_regMem(298,"飯田","いいだ");
+    regMem[52] = new set_regMem(94,"宮本","みやもと");
+    regMem[53] = new set_regMem(96,"松下","まつした");
+    regMem[54] = new set_regMem(301,"大朝J","おおともじ");
+    regMem[55] = new set_regMem(95,"澤田","さわだ");
+    regMem[56] = new set_regMem(0,"dummy","dummy");
+};
+
+function set_regMem(id, dispName, nickname){       //
+    this.id = id;
+    this.dispName = dispName;
+    this.nickname = nickname;
+}
+
+// IDから名前の取得（内部テーブルから）
+function get_NameFromTbl(idNum){
+    var i;
+    var retStr = null;
+    for(i in regMem){
+        if(regMem[i].id == idNum){
+//alert(i);
+            retStr = regMem[i].dispName;
+            break;
+        }
+    }
+    
+    return retStr;
+}
 
 //データをmobile backendに保存するメソッド
 function saveData(){
@@ -135,37 +217,6 @@ function makeDailyManageTbl(totalNum)
                     alert("makeDailyManageTbl Err :" + err);
                 })
 
-
-/*
-    var bkDate;
-    var totalDate = 0;
-    var scoreNum = 0;
-    var i;
-    
-    alert(scoreRslt[i].date + "  " + scoreRslt.count)
-    
-    for(i=0; i<scoreRslt.count; i++)
-    {
-        scoreNum++;
-        if(bkDate != scoreRslt[i].date)
-        {
-//            if(totalDate != 0)
-//            {
-//                saveDailyDate( scoreRslt[i].date, scoreNum/4 );
-//            }
-            totalDate++;
-            scoreNum=0;
-            bkDate = scoreRslt[i].date;
-        }
-        
-//        if(i==(scoreRslt.count-1))
-//        {
-//            saveDailyDate( scoreRslt[i].date, scoreNum/4 );
-//        }
-    }
-
-    alert("totalDate = " + totalDate);
-*/
 }
 
 function ChkDailyRefresh(ScoreLatest, resultManage)
@@ -274,12 +325,63 @@ function getDailyTblInfo()
         .then(function(dailyRslt){
             makeDailyMasterDisplay(dailyRslt);
         })
+         .catch(function(err){
+            alert("getDailyTblInfo error : " + err);
+        });
+   
+}
+
+/* 詳細画面作成 */
+function makeDetailTbl(rslt)
+{
+    var ptrNum = 0;
+    var Score = ncmb.DataStore( ThisScoreTbl );
+alert(rslt.date);
+    Score
+        .equalTo("date",rslt.date)
+        .order("gameNo")
+        .count()
+        .fetchAll()
+        .then(function(detailRslt){
+            makeDetailTbl_per100(detailRslt, ptrNum, rslt.date);
+        })
+        .catch(function(err){
+            alert("makeDetailTbl error : " + err);
+        });
     
 }
 
-
-
-
+/* 詳細画面作成 100ずつ */
+function makeDetailTbl_per100(detailRslt, ptrNum, date)
+{
+    var Score = ncmb.DataStore( ThisScoreTbl );
+    ptrNum += 100;
+    /* 全データ取得ならば */
+    if( detailRslt.count <= ptrNum )
+    {
+        makeDetailDisplay(detailRslt);
+    }
+    else
+    {
+    /* まだデータ必要ならば */
+        Score
+            .equalTo("date",date)
+            .order("gameNo")
+            .skip(ptrNum)
+            .fetchAll()
+            .then(function(detailRsltNext){
+                /* データをコピーして次の100データ取得 */
+                for(var i=0; i<detailRsltNext.length; i++)
+                {
+                    detailRslt[ptrNum+i] = detailRslt[i];
+                }
+                makeDetailTbl_per100(detailRslt, ptrNum, date);
+            })
+             .catch(function(err){
+                alert("makeDetailTbl_per100 error : " + err);
+            });
+    }
+}
 
 
 //mobile backendへの会員登録を行うメソッド
--- www/js/dailyRslt.js
+++ www/js/dailyRslt.js
@@ -10,9 +10,10 @@ document.addEventListener("pageinit", function(e) {
 //    document.getElementById("my-content").innerHTML = "Item A<br>";
         makeDailyTbl();
         
-  }else if(e.target.id == "detail.html")
+  }else if(e.target.id == "detailPage")
   {
-        
+        var page = dailyNavi.getCurrentPage();
+        makeDetailTbl(page.options.param1);
   }
 }, false);
 
@@ -25,7 +26,8 @@ function makeDailyMasterDisplay(dailyRslt)
             var onsListItem = document.createElement("ons-list");
             $rslt = dailyRslt[i];
 //              var onsListItem = document.createElement("my-content");
-            onsListItem.innerHTML = "<ons-row onclick = 'goToDailyDetailDisplay($rslt)'>" +
+            onsListItem.innerHTML = "<ons-row onclick = 'goToDailyDetailDisplay($rslt, this)'>" +
+//            onsListItem.innerHTML = "<ons-row onclick = '(function(){$tmpRslt=$rslt;goToDailyDetailDisplay($tmpRslt);})'>" +
                                         "<ons-col>"+
                                             "<header>"+dailyRslt[i].date
                                                     +" 試合数："
@@ -41,8 +43,58 @@ function makeDailyMasterDisplay(dailyRslt)
     
 }
 
-function goToDailyDetailDisplay(rslt)
+/* 詳細画面へ移行 */
+function goToDailyDetailDisplay(rslt, obj)
 {
     var options = {param1: rslt};
     dailyNavi.pushPage("page3.html", options);
+}
+
+/* 詳細画面作成 */
+function makeDetailDisplay(detailRslt)
+{
+//    alert("a" + detailRslt.count);
+    var name = ["","","",""];
+    var gamePt = [0,0,0,0];
+
+    var onsList = document.getElementById('detail-list');
+    for( var i = 0; i< detailRslt.length; i++ )
+    {
+        if(detailRslt[i].row != null)       //★nullのとき
+        {
+            var num = Number(detailRslt[i].ID);
+//alert(num);
+            name[detailRslt[i].row] = get_NameFromTbl(num);   //★テーブルないとき
+//alert(name[detailRslt[i].row]);
+            gamePt[detailRslt[i].row] = detailRslt[i].gamePt;
+        }
+        
+
+        
+        /* 4データで１ゲーム分の記述 */
+        if(i%4 == 3)
+        {
+            var onsListItem = document.createElement("detail-list");
+            onsListItem.innerHTML = "<ons-row>" +
+                                        "<ons-col>"+
+                                            "<header>"+"NO." + (i+1)/4 +
+                                            "</header>" +
+                                            "<header>"+"上段ゲーム数 = "
+                                                    +gamePt[0]
+                                                    + " |" 
+                                                    +name[0]+"さん,"
+                                                    +name[1]+"さん" +
+                                            "</header>"+
+                                            "<header>" +"下段ゲーム数 = "
+                                                    +gamePt[2]
+                                                    + " |" 
+                                                    +name[2]+"さん,"
+                                                    +name[3]+"さん"
+                                            "</header>"+
+                                        "</ons-col>"+
+                                    "</ons-row>";
+            onsList.appendChild(onsListItem);
+            ons.compile(onsListItem);
+        }
+    }
 }
